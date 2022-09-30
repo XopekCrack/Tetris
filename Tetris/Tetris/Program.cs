c554c7d6f0698c8442ee65b8f88177df57a86f2e
LazevizCrack <aulsin845@gmail.com>
2022-09-30 15:26:27 +0300

    Вывод * на экран

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,16 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+            Console.ReadLine();
+            Point p1 = new Point();
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
+            p1.Draw();
+
+            Console.ReadLine();
         }
     }
 }
